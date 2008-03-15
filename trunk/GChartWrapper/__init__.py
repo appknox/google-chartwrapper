8b83ef82343064b9d6eb95c4701dd3dfb4791ba2
justquick <justquick@ab4c0b43-f447-0410-ac63-8f6eca9f32e0>
2008-03-15 18:14:24 +0000

    added django extension

--- trunk/GChartWrapper/__init__.py
+++ trunk/GChartWrapper/__init__.py
@@ -46,44 +46,33 @@ from webbrowser import open as webopen
 from GChartWrapper.constants import *
 from GChartWrapper.encoding import Encoder
 
-class Axes(object):
+class Axes(UserDict):
     """Axes attr storage"""
     def __init__(self):
-        self._type = ''
         self.labels = []
-        self.positions = []
-        self.ranges = [] 
-        self.styles = []
-        self._lid = None
-        self._sid = None        
+        self.styles = []    
+        UserDict.__init__(self)
     def render(self):
-        data = {}
-        if self._type:
-            data['chxt'] = ','.join(self._type)
         if self.labels:            
-            data['chxl'] = '|'.join(self.labels)   
-        if self.positions:                 
-            data['chxp'] = '|'.join(self.positions)  
-        if self.ranges:
-            data['chxp'] = '|'.join(self.ranges)  
+            self.data['chxl'] = '|'.join(self.labels)   
         if self.styles:
-            data['chxs'] = '|'.join(self.styles)              
-        return data
+            self.data['chxs'] = '|'.join(self.styles)              
+        return self.data
     def label(self, *args):
-        assert(self._lid != None), 'Need to call type before labeling'
-        self.labels.append('%d:|%s'%(self._lid,'|'.join(map(str,args))))
-        self._lid += 1
+        label = '|'.join(map(str,args))
+        id = len(self.labels)
+        self.labels.append( str('%d:|%s'%(id,label)).replace('None','') )
     def position(self, *args):
-        self.positions.append(','.join(map(str,args)))
+        self.data['chxp'] = ','.join(map(str,args))
     def range(self, *args):
-        self.ranges.append('%d,%d,%d'%args)
-    def type(self, typ):
-        self._lid = 0
-        self._sid = 0
-        self._type = typ.replace(',','')     
+        self.data['chxr'] = '%d,%d,%d'%args
+    def type(self, atype):
+        if not ',' in atype:
+            atype = ','.join(atype)
+        self.data['chxt'] = atype
     def style(self, *args):  
-        assert(self._sid != None), 'Need to call type before labeling'
-        self.styles.append(','.join([str(self._sid)]+map(str,args)))
+        id = str(len(self.styles))
+        self.styles.append(','.join([id]+map(str,args)))
         
 class GChart(UserDict):
     """Main chart class
@@ -91,112 +80,145 @@ class GChart(UserDict):
     chart type must be valid for cht parameter
     dataset can be any python iterable
     kwargs will be put into chart params if valid"""
-    def __init__(self, ctype, dataset, **kwargs):
-        self._type = ctype
-        self.labels = []
-        self._title = ''
-        self.title_style = ''
-        self.legends = []
-        self.colors = []  
-        self.bars = []  
+    def __init__(self, ctype=None, dataset=[], **kwargs):
         self.lines = []
-        self.grids = []
+        self.fills = []
         self.markers = []
-        self.dataset = dataset
+        self._dataset = dataset
+        self.axes = Axes()        
         UserDict.__init__(self)
-        self.encoding = 'simple'
-        self.maxValue = None
+        if ctype:
+            self.check_type(ctype)
+            self.data['cht'] = ctype
+        self._scale = None
+        if 'scale' in kwargs:
+            self._scale = float(kwargs['scale'])
+            del kwargs['scale']            
+        self._encoding = None
         if 'encoding' in kwargs:
-            self.encoding = kwargs['encoding']
-            del kwargs['encoding']
-        self.encoder = Encoder(self.encoding)            
-        if 'maxValue' in kwargs:
-            self.maxValue = kwargs['maxValue']
-            del kwargs['maxValue']         
+            self._encoding = kwargs['encoding']
+            del kwargs['encoding']            
+
+        self.apiurl = 'http://chart.apis.google.com/chart?'                
         if 'apiurl' in kwargs:
             self.apiurl = kwargs['apiurl']
-            del kwargs['apiurl']                  
-        else:
-            self.apiurl = 'http://chart.apis.google.com/chart?'                    
-        self.axes = Axes()
-        self.data.update(cht=ctype)        
-        for k,v in kwargs:
+            del kwargs['apiurl']                     
+        for k,v in kwargs.items():
             assert(k in APIPARAMS), 'Invalid chart parameter: %s'%k                
             self.data[k] = v
+       
+    def encoding(self, encoding): 
+        self._encoding = encoding
+
+    def scale(self, scale):
+        self._scale = float(scale)
+
+    def dataset(self, data):
+        self._dataset = data                   
+       
     def marker(self, *args):
         assert(args[0] in MARKERS), 'Invalid marker type: %s'%args[0]
         assert(len(args) == 5), 'Incorrect arguments %s'%str(args)
         self.markers.append(','.join(map(str,args)) )
+        
     def line(self, *args):
-        self.lines.append(','.join(['%.1f'%x for x in args]))
+        self.lines.append(','.join(['%.1f'%x for x in map(float,args)]))
+        
+    def fill(self, *args):
+        assert(args[0] in ('c','bg')), 'Fill must be bg/c not %s'%args[0]
+        assert(args[1] in ('s','lg','ls')), 'Fill type must be s/lg/ls not %s'%args[1]
+        self.fills.append(','.join(map(str,args)))
+        
     def grid(self, *args):
-        self.grids = map(float,args)             
+        grids =  map(str,map(float,args))
+        self.data['chg'] = ','.join(grids).replace('None','')          
+        
     def bar(self, *args):
-        self.bars = map(int,args)                 
-    def color(self, *args):
-        self.colors = list(args)       
-    def type(self, type):
-        self._type = type      
+        bars = ['%.1f'%b for b in map(float,args)]        
+        self.data['chbh'] = ','.join(bars).replace('None','')
+
+    def color(self, *args): 
+        self.data['chco'] = ','.join(args)  
+
+    def type(self, type):        
+        self.data['cht'] = str(type)
+        
     def label(self, *args):
-        self.labels = list(args)    
+        self.data['chl'] = '|'.join(args)   
+        
     def legend(self, *args):
-        self.legends = list(args)
+        self.data['chdl'] = '|'.join(args)
+       
     def title(self, title, *args): 
-        self._title = title
+        self.data['chtt'] = title        
         if args:
-            self.title_style = ','.join(map(str,args))
+            self.data['chts'] = ','.join(map(str,args))
+            
     def render(self):
         """Renders the chart context and axes into the dict data"""
+        self.data.update(**self.axes.render())     
+        encoder = Encoder(self._encoding)  
         if not 'chs' in self.data:
-            self.size(300,150)
-        else:
-            self.checkSize(*map(int,self.data['chs'].split('x')))
-        assert('cht' in self.data), 'No chart type (cht) in data'
-        assert(self.data['cht'] in TYPES), 'Invalid chart type: %s'%self.data['cht']    
-        self.data['chd'] = self.encoder.encode(self.dataset, self.maxValue)
-        if self.labels:
-            self.data['chl'] = '|'.join(self.labels)     
-        if self._title:
-            self.data['chtt'] = self._title    
-        if self.title_style:
-            self.data['chts'] = self.title_style
-        if self.legends:
-            self.data['chdl'] = '|'.join(self.legends)                   
-        if self.colors:
-            self.data['chco'] = ','.join(self.colors)                
-        if self.bars:
-            self.data['chbh'] = ','.join(map(str,self.bars))
+            self.data['chs'] = '300x150'
+        else:            
+            size = self.data['chs'].split('x')
+            assert(len(size) == 2), 'Invalid size, must be in the format WxH'
+            self.check_size(*map(int,size))
+        assert('cht' in self.data), 'No chart type defined, use type method'
+        self.data['cht'] = self.check_type(self.data['cht'])   
+        if self._dataset:
+            self.data['chd'] = encoder.encode(self._dataset,scale=self._scale)             
+        else: pass
+            #assert('chd' in self.data), 'You must have a dataset, or use chd'            
         if self.lines:
-            self.data['chls'] = '|'.join(self.lines) 
-        if self.grids:
-            self.data['chg'] = ','.join(map(str,self.grids))
+            self.data['chls'] = '|'.join(self.lines)            
         if self.markers:
             self.data['chm'] = '|'.join(self.markers)           
-        self.data.update(**self.axes.render())            
-
-    def getData(self):
-        """Returns encoded dataset"""
-        if 'chd' in self.data:
-            return self.data['chd']        
-    def checkSize(self,x,y):
+        if self.fills:
+            self.data['chf'] = '|'.join(self.fills)
+       
+                          
+    def check_size(self,x,y):
         """Make sure the chart size fits the standards"""
         assert(x <= 10**3), 'Width larger than 1000'         
         assert(y <= 10**3), 'Height larger than 1000'        
-        assert(x*y <= 3*(10**5)), 'Resolution larger than 300000'         
+        assert(x*y <= 3*(10**5)), 'Resolution larger than 300000' 
+          
+    def check_type(self, type):
+        """Check to see if the type is either in TYPES or fits type name
+        
+        Returns proper type"""
+        tdict = dict(zip(TYPES,TYPES))
+        tdict['line'] = 'lc'
+        tdict['bar'] = 'bvs'
+        tdict['pie'] = 'p'
+        tdict['venn'] = 'v'
+        tdict['scater'] = 's'
+        assert(type in tdict), 'Invalid chart type: %s'%type  
+        return tdict[type]
+             
     def size(self,*args):
         """Set the size of the chart, args are width,height and can be tuple"""
         if len(args) == 2:
             x,y = map(int,args)
         else:
             x,y = map(int,args[0])
-        self.checkSize(x,y)
+        self.check_size(x,y)
         self.data['chs'] = '%dx%d'%(x,y)  
-    def __repr__(self):
-        return '<GChart %s %s %s>'%(self._title,self._type,self.dataset)
+
+    def getname(self):
+        """Gets the name of the chart, if it exists"""
+        if 'chtt' in self.data:
+            return self.data['chtt']
+    
+    def getdata(self):
+        return Encoder().decode(self.data['chd'])        
+        
+    def __repr__(self):  self.__str__()
     def __str__(self):
         """Returns the rendered URL of the chart"""
         self.render()
-        params = '&'.join(['%s=%s'%x for x in self.data.items()])
+        params = '&'.join(['%s=%s'%x for x in self.data.items() if x[1]])
         return self.apiurl + params.replace(' ','+')
            
     def show(self):
@@ -206,12 +228,9 @@ class GChart(UserDict):
     def save(self, fname=None):
         """Download the chart from the URL into a filename as a PNG
         
-        The filename defaults to the chart title if any"""
-        if fname == None:
-            if 'chtt' in self.data:
-                fname = self.data['chtt']
-            elif type(self._title)==type('') and self._title:
-                fname = self.data['chtt'] = self._title                
+        The filename defaults to the chart title (chtt) if any"""
+        if not fname:
+            fname = self.getname()               
         assert(fname != None), 'You must specify a filename to save to'
         if not fname.endswith('.png'):
             fname += '.png'
@@ -227,9 +246,13 @@ class GChart(UserDict):
         kwargs can be other img tag attributes, which are strictly enforced"""
         attrs = ''
         href = str(self)
-        title = self.data.get('chtt','')
+        title = self.getname()
         for item in kwargs.items():
             if not item[0] in IMGATTRS:
                 raise AttributeError, 'Invalid img tag attribute: %s'%item[0]
             attrs += '%s="%s" '%item
         return '<img alt="%s" title="%s" src="%s" %s>'%(title,title,href,attrs)
+        
+if __name__=='__main__':
+    from GChartWrapper.tests import test
+    test()
--- trunk/GChartWrapper/encoding.py
+++ trunk/GChartWrapper/encoding.py
@@ -1,70 +1,125 @@
-alphanum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
 class Encoder:
     """Data encoder that handles simple,text, and extended encodings
     
-    Based on javascript encoding algorithm"""
-    def __init__(self, encoding, maxValue=None):        
-        self.maxValue = maxValue
-        assert(encoding in ('simple','text','extended')),\
-            'Unknown encoding: %s'%encoding    
-        self.encoding = encoding
-        if encoding == 'simple':
-            self.char = ','
-            self.coding = alphanum
-            self._encode = self.simple
+    Based on javascript encoding algorithm and pygooglecharts"""
+    def __init__(self, encoding=None): 
+        self.encoding = 'extended'
+        if encoding:
+            assert(encoding in ('simple','text','extended')),\
+            'Unknown encoding: %s'%encoding        
+            self.encoding = encoding          
+        coding = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        ecoding = coding + '-.'
+        if self.encoding == 'simple':
+           self.coding =  coding
+           self.max_value =  61
+           self.char = ','
+           self.dchar = ''
+           self.none = '_'                
+           self.value = lambda x: coding[x]            
         elif self.encoding == 'text':
-            self.char = '|'             
-            self._encode = self.text    
-        elif encoding == 'extended':
-            self.char = ','
-            self.coding = alphanum + '-.'
-            self._encode = self.extended     
+           self.coding = ''
+           self.max_value =  100
+           self.none = '-1'
+           self.char = '|'
+           self.dchar = ','
+           self.value = lambda x: '%.1f'%x                          
+        elif self.encoding == 'extended':
+           self.coding =  ecoding
+           self.max_value =  4095
+           self.none =  '__'
+           self.dchar = ''
+           self.char = ','         
+           self.value = lambda x: '%s%s'% \
+                (ecoding[int(float(x)/64)], ecoding[x%64])
+    
+    def scalevalue(self, value):
+        if self.scale and type(self.scale) == type(()):
+            lower,upper = self.scale
+        elif self.scale:
+            lower,upper = 0,float(self.scale)
+        else:
+            lower,upper = 0,self.max_value
+        scaled = int(round(float(value - lower) * self.max_value / upper))
+        return min(scaled, self.max_value)
 
-    def encode(self,dataset,maxValue=None):
+    def encode(self,  *args, **kwargs):
         """Encode wrapper for a dataset with maximum value
         
-        Datasets can be two dimensional
+        Datasets can be one or two dimensional
         Strings are ignored as ordinal encoding"""
-        if maxValue == None:
-            if self.maxValue:
-                maxValue = self.maxValue
-        code = self.encoding[0]+':'
-        if type(dataset[0])==type([]):              
-            data = self.char.join([self._encode(data, maxValue) for data in dataset])
+        if type(args[0]) == type(''):
+            return self.encode([args[0]],**kwargs)
+        if len(args)>1:
+            dataset = args
         else:
-            data = self._encode(dataset, maxValue)
+            dataset = args[0]  
+        typemap = map(type,dataset)
+        code = self.encoding[0]+':'
+        if type('') in typemap:  
+            return code + ','.join(map(str,dataset))
+        self.scale = None      
+        if 'scale' in kwargs:
+            self.scale = kwargs['scale']                 
+        if type([]) in typemap:  
+            data = self.char.join([self.encodedata(data) for data in dataset]) 
+        elif len(dataset) == 1:
+            data = self.encodedata(dataset[0])                                  
+        else:            
+            data = self.encodedata(dataset)        
         return code + data            
-        
-    def simple(self,valueArray,maxValue):                       
-        if type(valueArray[0]) == type(''):  
-            return ','.join(valueArray)        
-        if maxValue == None:
-            maxValue = max(valueArray)
-        chartData = []         
-        for currentValue in valueArray:
-            if currentValue != None and currentValue >= 0:
-                n = round(61 * float(currentValue) / float(maxValue))
-                chartData.append(self.coding[int(n)])
-            else:
-                chartData.append('_')   
-        return ''.join(chartData)            
-    def extended(self,valueArray,maxValue):
-        #XXX: alot about this doesnt seem right, not well tested
-        chartData = ''
-        if maxValue == None:
-            maxValue = max(valueArray)        
-        for currentValue in valueArray:
-            if currentValue != None and currentValue >= 0:
-                chartData += self.coding[int(float(currentValue)/64)]
-                chartData += self.coding[currentValue%64]
-            else:
-                chartData += '__'   
-        return chartData
-    def text(self,valueArray,junk):
-        chartData = []
-        for currentValue in valueArray:            
-            if currentValue != None and (0 <= currentValue <= 100):
-                chartData.append('%.1f'%currentValue)
-            else:
-                chartData.append('-1')                
-        return ','.join(chartData)
+
+    def encodedata(self, data):
+        sub_data = []
+        enc_size = len(self.coding)
+        for value in data: 
+            if value in (None,'None'):
+                sub_data.append(self.none)
+            elif value >= 0:
+                try:
+                    sub_data.append(self.value(self.scalevalue(value)))
+                except ValueError:
+                    raise ValueError, 'cannot encode value: %s' % value                         
+        return self.dchar.join(sub_data)
+
+    def decode(self, astr):
+        e = astr[0]
+        dec_data = []
+        for data in astr[2:].split(self.char):
+            sub_data = []
+            if e == 't':
+                for value in data.split(','):
+                    sub_data.append(float(value))
+            elif e == 'e':
+                flag = 0
+                index = self.coding.index
+                for i in range(len(data)):
+                    if not flag:
+                        this,next = index(data[i]),index(data[i+1])
+                        flag = 1
+                        sub_data.append((64 * this) + next)  
+                    else: flag = 0
+            elif e == 's':                        
+                for value in data:
+                    sub_data.append(self.coding.index(value))
+            dec_data.append(sub_data)                    
+        return dec_data
+
+def test():
+    
+    for q,a,d in [
+        ('simple','s:Ab9',[0,27,61]),
+        ('text','t:0.0,10.0,100.0',[0,10,100]),
+        ('extended','e:AH-HAA..',[7,3975,0,4095])
+        ]:
+        E = Encoder(q)
+        data = [d]
+        test = E.encode(data)
+        print test
+        assert(E.decode(test) == data)    
+        assert(a == test)
+    
+   
+if __name__=='__main__': test()    
+    
