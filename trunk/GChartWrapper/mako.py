153ed199764206e3aedb55dbd19be41baa057b54
justquick <justquick@ab4c0b43-f447-0410-ac63-8f6eca9f32e0>
2009-01-05 14:48:54 +0000

    mako func moved to __init__

--- trunk/GChartWrapper/mako.py
+++
@@ -1,6 +0,0 @@
-import GChartWrapper
-
-def chart(context, chart=None, *args, **kwargs):
-    if chart and chart in dir(GChartWrapper):
-        return getattr(GChartWrapper, chart)(*args, **kwargs)
-    return GChartWrapper.GChart(*args, **kwargs)
