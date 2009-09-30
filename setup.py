2cc7da292c8901a7e422741a884559b2995875b5
justquick <justquick@ab4c0b43-f447-0410-ac63-8f6eca9f32e0>
2009-09-30 19:02:05 +0000

    changed primary license to bsd, added newdoc and comitted v0.9

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ CLASSIFIERS = (
 
 setup(
     name='GChartWrapper',
-    version='0.8',
+    version='0.9',
     description='Python Google Chart Wrapper',
     long_description="""Python wrapper for the Google Chart API. 
 The wrapper can render the URL of the Google chart, based on your parameters, 
@@ -49,7 +49,7 @@ instance of the chart for manipulation. Works for Python 2 and 3""",
     author="Justin Quick",
     author_email='justquick@gmail.com',
     url='http://code.google.com/p/google-chartwrapper/',
-    download_url='http://google-chartwrapper.googlecode.com/files/GChartWrapper-0.8.tar.gz',
+    download_url='http://google-chartwrapper.googlecode.com/files/GChartWrapper-0.9.tar.gz',
     platforms = ["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
     classifiers=CLASSIFIERS,
       package_dir={'GChartWrapper': 'GChartWrapper'},
