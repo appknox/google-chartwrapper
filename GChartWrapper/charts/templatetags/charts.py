b5bc69bfc0f4ffd7f12ca07fc828999e01e6769b
Roland van Laar <roland@micite.net>
2018-12-20 14:55:55 +0100

    Fixes for use with Python 3
    drops python < 2.6 support.

--- GChartWrapper/charts/templatetags/charts.py
+++ GChartWrapper/charts/templatetags/charts.py
@@ -34,14 +34,9 @@ from django.template import Library,Node
 from django.template import Variable, VariableDoesNotExist
 import GChartWrapper
 
-# version independent testing for numeric types
-try:
-    # python2.6 and later
-    from numbers import Number as NumberType
-    isNumberType = lambda x: isinstance(x, NumberType)
-except ImportError, e:
-    # deprecated function
-    from operator import isNumberType
+# python2.6 and later
+from numbers import Number as NumberType
+isNumberType = lambda x: isinstance(x, NumberType)
 
 register = Library()
 
